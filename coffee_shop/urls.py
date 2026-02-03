e5355d64ecf4bb73e41d063a3109b158df86d34e
Karen Vargas <kalivargas.11@gmail.com>
2026-02-03 11:54:32 -0500

    doc: Agregar README del proyecto

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
+    path('', include('products.urls')),
     path('admin/', admin.site.urls),
     path('productos/', include('products.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
