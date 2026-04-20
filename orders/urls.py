8e511243ee28200c9e98d2eeefab2c1854b5c920
Karen Vargas <kalivargas.11@gmail.com>
2026-04-20 13:38:40 -0500

    feat: Finalización ejercicio, crear productos, ordenar

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,10 @@
+from django.urls import path
+from .views import MyOrderView, CreateOrderProductView
 
 from django.urls import path
 urlpatterns = [
- 
+    path('mi-orden/', MyOrderView.as_view(), name="my_order"),
+    path('agregar-producto', CreateOrderProductView.as_view(), name="add_order")
+    
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from decimal import Decimal
+from django.views.generic import DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
+from django.urls     import reverse_lazy
+from .models import Orders
+from .forms import OrderProductForm
 
-# Create your views here.
+
+class MyOrderView(LoginRequiredMixin,DetailView):
+    model = Orders
+    template_name = 'orders/my_order.html'
+    context_object_name = 'order'
+
+    def get_object(self):
+        order = Orders.objects.filter(is_active=True, user = self.request.user).first()
+        if not order:
+            raise Http404("No hay una orden activa.")
+        return order
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        order = self.object
+
+        subtotal = Decimal("0.00")
+        order_items = order.orderproduct_set.all()
+
+        for item in order_items:
+            item.subtotal_item = item.product.price * item.quantity
+            subtotal += item.subtotal_item
+
+        igv = subtotal * Decimal("0.18")
+        total_order = subtotal + igv
+
+        context['order_items'] = order_items
+        context['subtotal'] = subtotal
+        context['igv'] = igv
+        context['total_order'] = total_order
+
+        return context
+
+
+class CreateOrderProductView(LoginRequiredMixin,CreateView):
+    template_name = 'orders/create_order_product.html'
+    form_class = OrderProductForm
+    success_url = reverse_lazy('my_order')
+
+    def form_valid(self, form):
+        order, created = Orders.objects.get_or_create(
+            is_active = True,
+            user = self.request.user,
+        )
+        form.instance.order = order
+        form.instance.quantity = 1
+        form.save()
+        return super().form_valid(form)
